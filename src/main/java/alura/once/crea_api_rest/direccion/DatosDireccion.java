6e9423849cfce7b4e9f1d42ec149c96275053ff2
Veronica <vgalbicka@gmail.com>
2024-06-03 13:55:20 -0300

    Create DatosDireccion.java

---
+++ src/main/java/alura/once/crea_api_rest/direccion/DatosDireccion.java
@@ -0,0 +1,10 @@
+package alura.once.crea_api_rest.direccion;
+
+public record DatosDireccion(
+        String calle,
+        String distrito,
+        String ciudad,
+        String numero,
+        String complemento
+) {
+}
