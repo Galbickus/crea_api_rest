c7a8e7d0fab0da1de0d0fe6f0b56eddfe657ed03
Veronica <vgalbicka@gmail.com>
2024-06-03 13:55:36 -0300

    Update DatosRegistroMedicos.java

--- src/main/java/alura/once/crea_api_rest/medico/DatosRegistroMedicos.java
+++ src/main/java/alura/once/crea_api_rest/medico/DatosRegistroMedicos.java
@@ -1,12 +1,12 @@
 package alura.once.crea_api_rest.medico;
 
-import alura.once.crea_api_rest.DatosDireccion;
+import alura.once.crea_api_rest.direccion.DatosDireccion;
 
 public record DatosRegistroMedicos(
         String nombre,
         String email,
         String documento,
         Especialidad especialidad,
-        DatosDireccion direccion
+        DatosDireccion datosDireccion
 ) {
 }
