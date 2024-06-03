bde0f72eed3da6d634170925eccabc848618b241
Veronica <vgalbicka@gmail.com>
2024-06-03 18:43:58 -0300

    Update PacienteController.java

--- src/main/java/alura/once/crea_api_rest/controller/PacienteController.java
+++ src/main/java/alura/once/crea_api_rest/controller/PacienteController.java
@@ -1,9 +1,16 @@
 package alura.once.crea_api_rest.controller;
 
 import alura.once.crea_api_rest.dto.DatosRegistroPaciente;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("pacientes")
 public class PacienteController {
-    public void registrar(DatosRegistroPaciente datos){
+    @PostMapping
+    public void registrar(@RequestBody DatosRegistroPaciente datos){
         System.out.println("datos recibidos: " + datos);
     }
 }
