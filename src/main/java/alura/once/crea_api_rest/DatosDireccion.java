ed0adcf393dd24f3bb24ff143e14825264320624
Veronica <vgalbicka@gmail.com>
2024-06-03 11:31:09 -0300

    Crea record datos direccion para reutilizar
    Se va a usar en medicos pero tambien en cualquier persona por eso lo separamos para reutilizar

---
+++ src/main/java/alura/once/crea_api_rest/DatosDireccion.java
@@ -0,0 +1,10 @@
+package alura.once.crea_api_rest;
+
+public record DatosDireccion(
+        String calle,
+        String distrito,
+        String ciudad,
+        String numero,
+        String complemento
+) {
+}
