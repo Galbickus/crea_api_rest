b45827030fb8222c06fec715009c352a48e46fad
Veronica <vgalbicka@gmail.com>
2024-06-03 13:54:24 -0300

    Update MedicoController.java

--- src/main/java/alura/once/crea_api_rest/controller/MedicoController.java
+++ src/main/java/alura/once/crea_api_rest/controller/MedicoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
     @PostMapping
     public void registrarMedico(@RequestBody DatosRegistroMedicos datosRegistroMedicos){
-        System.out.println("El request llega correctamente");
+
         System.out.println(datosRegistroMedicos);
 
     }
