9557d7e766cd6343992974923cc8c8418713adb5
Veronica <vgalbicka@gmail.com>
2024-06-03 12:50:17 -0300

    Update MedicoController.java

--- src/main/java/alura/once/crea_api_rest/controller/MedicoController.java
+++ src/main/java/alura/once/crea_api_rest/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package alura.once.crea_api_rest.controller;
 
+import alura.once.crea_api_rest.medico.DatosRegistroMedicos;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
     @PostMapping
-    public void registrarMedico(@RequestBody String parametro){
+    public void registrarMedico(@RequestBody DatosRegistroMedicos datosRegistroMedicos){
         System.out.println("El request llega correctamente");
-        System.out.println(parametro);
+        System.out.println(datosRegistroMedicos);
 
     }
 }
