c3644503f7f26d19d5697ffe24361f7424153fc0
Veronica <vgalbicka@gmail.com>
2024-06-04 22:52:43 -0300

    Update Direccion.java

--- src/main/java/alura/once/crea_api_rest/direccion/Direccion.java
+++ src/main/java/alura/once/crea_api_rest/direccion/Direccion.java
@@ -1,10 +1,10 @@
 package alura.once.crea_api_rest.direccion;
 
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-@Table(name = "medicos")
-@Entity(name = "Medico")
+@Embedded
 public class Direccion {
     private String calle;
     private Integer numero;
