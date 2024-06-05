1631bf45de5b77f0fe8ad9bdce1d136964609dc3
Veronica <vgalbicka@gmail.com>
2024-06-04 22:52:55 -0300

    Update Medico.java

--- src/main/java/alura/once/crea_api_rest/medico/Medico.java
+++ src/main/java/alura/once/crea_api_rest/medico/Medico.java
@@ -2,12 +2,19 @@ package alura.once.crea_api_rest.medico;
 
 import alura.once.crea_api_rest.direccion.DatosDireccion;
 import alura.once.crea_api_rest.direccion.Direccion;
+import jakarta.persistence.*;
 
+@Table(name = "medicos")
+@Entity(name = "Medico")
 public class Medico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
    private  String nombre;
     private String email;
     private String documento;
+    @Enumerated(EnumType.STRING)
     private Especialidad especialidad;
-    private alura.once.crea_api_rest.direccion.Direccion Direccion;
+    @Embedded
+    private Direccion direccion;
 }
