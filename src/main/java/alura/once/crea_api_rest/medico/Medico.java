fd29d552e3b1d3be12f8334a4e136374d63fd342
Veronica <vgalbicka@gmail.com>
2024-06-04 23:05:28 -0300

    Update Medico.java

--- src/main/java/alura/once/crea_api_rest/medico/Medico.java
+++ src/main/java/alura/once/crea_api_rest/medico/Medico.java
@@ -3,9 +3,17 @@ package alura.once.crea_api_rest.medico;
 import alura.once.crea_api_rest.direccion.DatosDireccion;
 import alura.once.crea_api_rest.direccion.Direccion;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Table(name = "medicos")
 @Entity(name = "Medico")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Medico {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
