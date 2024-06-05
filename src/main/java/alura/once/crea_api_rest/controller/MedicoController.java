c15d682b58b098f7265448488d8bd7554867f1d3
Veronica <vgalbicka@gmail.com>
2024-06-05 00:53:47 -0300

    Update MedicoController.java

--- src/main/java/alura/once/crea_api_rest/controller/MedicoController.java
+++ src/main/java/alura/once/crea_api_rest/controller/MedicoController.java
@@ -1,6 +1,9 @@
 package alura.once.crea_api_rest.controller;
 
 import alura.once.crea_api_rest.medico.DatosRegistroMedicos;
+import alura.once.crea_api_rest.medico.Medico;
+import alura.once.crea_api_rest.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,10 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/medicos")
 public class MedicoController {
+    @Autowired
+    private MedicoRepository medicoRepository;
     @PostMapping
     public void registrarMedico(@RequestBody DatosRegistroMedicos datosRegistroMedicos){
 
-        System.out.println(datosRegistroMedicos);
+        medicoRepository.save(new Medico(datosRegistroMedicos));
 
     }
 }
