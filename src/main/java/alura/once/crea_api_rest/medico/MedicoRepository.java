e02fc85ebabef403f1daeeea781d585f21a5254f
Veronica <vgalbicka@gmail.com>
2024-06-05 00:52:04 -0300

    Create MedicoRepository.java

---
+++ src/main/java/alura/once/crea_api_rest/medico/MedicoRepository.java
@@ -0,0 +1,6 @@
+package alura.once.crea_api_rest.medico;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicoRepository extends JpaRepository<Medico,Long> {
+}
