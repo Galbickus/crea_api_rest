0675ae369fd9729aa0c2b141166c4719f9371c9e
Veronica <vgalbicka@gmail.com>
2024-06-05 00:53:11 -0300

    Update Direccion.java

--- src/main/java/alura/once/crea_api_rest/direccion/Direccion.java
+++ src/main/java/alura/once/crea_api_rest/direccion/Direccion.java
@@ -14,8 +14,17 @@ import lombok.NoArgsConstructor;
 @Embeddable
 public class Direccion {
     private String calle;
-    private Integer numero;
+    private String numero;
     private String complemento;
     private String distrito;
     private String ciudad;
+
+    public Direccion(DatosDireccion direccion) {
+        this.calle = direccion.calle();
+        this.numero = direccion.numero();
+        this.complemento = direccion.complemento();
+        this.distrito = direccion.distrito();
+        this.ciudad = direccion.ciudad();
+
+    }
 }
