2cad7ea7a082ec8b979ce0cb27dae4ad21f20227
Veronica <vgalbicka@gmail.com>
2024-06-04 21:18:09 -0300

    Create Medico.java

---
+++ src/main/java/alura/once/crea_api_rest/medico/Medico.java
@@ -0,0 +1,13 @@
+package alura.once.crea_api_rest.medico;
+
+import alura.once.crea_api_rest.direccion.DatosDireccion;
+import alura.once.crea_api_rest.direccion.Direccion;
+
+public class Medico {
+    private Long id;
+   private  String nombre;
+    private String email;
+    private String documento;
+    private Especialidad especialidad;
+    private alura.once.crea_api_rest.direccion.Direccion Direccion;
+}
